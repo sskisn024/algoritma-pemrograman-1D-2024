91a3bdbdd7e45af349fb67ae7049dc8b78c9a8ab
24-127_DIMAS-IZZULHAQQ-BUSTHOMI <dimasizzul85@gmail.com>
2024-10-01 20:28:04 +0700

    240441100127_DIMASIZZULHAQQBUSTHOMI_MODUL1_AsprakKakSarah

---
+++ modul 1/NomerDua.py
@@ -0,0 +1,47 @@
+# NOMER 2
+
+suku_5 = 11 
+suku_8_12 = 52
+d = 3
+a = 11 - 4 * d # Suku Pertama
+n = 8 # Jumlah suku pertama
+JumalahSukuPertama = (n / 2)*(2 * a + (n - 1) * d)
+print("Jumlah nilai dari 8 suku pertama adalah",JumalahSukuPertama)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Persamaan 1 : a + 4d = 11
+# Persamaan 2 : (a + 7d) + (a + 11d) = 52
+# Dari persamaan 1 : a = 11 - 4d
+# Subtitusi persamaan 2 : 
+# (11 - 4d + 7d ) + (11 - 4d + 11d) = 52
+# 11 + 3d + 11 + 7d = 52
+# 22 + 10d = 52
+# 10d = 30
+# d = 3

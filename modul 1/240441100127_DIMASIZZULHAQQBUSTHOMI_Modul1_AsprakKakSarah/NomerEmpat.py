a8fc2aed99371638d45ce864fe7b6b08d0e7c96f
dimasizzulhaqqbusthomi <dimasizzul85@gmail.com>
2024-10-01 20:11:42 +0700

    240441100127_DIMASIZZULHAQQBUSTHOMI_Modul1_AsprakKakSarah

---
+++ modul 1/240441100127_DIMASIZZULHAQQBUSTHOMI_Modul1_AsprakKakSarah/NomerEmpat.py
@@ -0,0 +1,46 @@
+# NOMER 4
+
+Vactorial7 = 7*6*5*4*3*2
+Vactorial4 = 4*3*2
+Vactorial3 = 3*2
+
+hasilPembagian1 = Vactorial7 // Vactorial4
+hasilVactorial = hasilPembagian1 // Vactorial3
+
+
+print("Jadi, ada", hasilVactorial ,"cara untuk memilih 4 orang dari 7 orang")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# C(7,4) = 7! / 4! (7 - 4)! 
+#        = 7! / 4! * 3! 
+# 7! = 7*6*5*4!
+# 4! = 4*3*2! = 24
+# 3! = 3*2! = 6
--- modul 1/DimasIzzulhaqqBusthomi.py
+++
@@ -1,3 +0,0 @@
-print("Nama : Dimas Izzulhaqq Busthomi")
-print("Nim : 240441100127")
-print("Kelas : Algortima Pemrograman 1D-2024")

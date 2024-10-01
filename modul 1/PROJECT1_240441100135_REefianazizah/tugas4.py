82ddf0ac2b870e03370f383c9843d6274ccfd60d
Refianaazizah <refianaazizah01@gmail.com>
2024-10-01 06:10:32 -0700

    Update tugas4.py

---
+++ modul 1/PROJECT1_240441100135_REefianazizah/tugas4.py
@@ -0,0 +1,14 @@
+# c(7,4) = 7! / 4! (7-4) !
+    #    = 7! / 4! * 3!
+
+# 7! = 7*6*5*4
+# 4! = 4*3*2
+# 3! = 3*2
+# C(7,4) =
+
+faktorial_7 = 7*6*5
+faktorial_3 = 3*2
+
+hasil_faktorial = faktorial_7 / faktorial_3
+
+print("jadi hasil", hasil_faktorial, "cara untuk memilih 4 orang dari 7 orang")

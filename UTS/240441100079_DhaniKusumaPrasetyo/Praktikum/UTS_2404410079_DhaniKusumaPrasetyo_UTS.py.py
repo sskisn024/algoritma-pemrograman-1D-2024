e260046bdf816c649fbce8f5c80a99a50645aa55
DHANIKUSUMA <dhaniksmpr@gmail.com>
2024-10-16 23:25:25 +0700

    240441100079_DhaniKusumaPrasetyo_UTS (#119)

---
+++ UTS/240441100079_DhaniKusumaPrasetyo/Praktikum/UTS_2404410079_DhaniKusumaPrasetyo_UTS.py.py
@@ -0,0 +1,22 @@
+makan = input("apakah kamu sudah makan ? ")
+mandi = input("apakah kamu sudah mandi ?")
+transportasi = input("ke kampus naik apa")
+ 
+if makan == "tidak":
+    print("makanlah selama 15 menit")
+else:
+    print("bagus waktu kamu bertambah")
+
+if mandi == "tidak ":
+    print("mandilah selama 10 menit")
+else:
+    print("bagus waktumu bertambah")
+
+if transportasi == "motor":
+    print ("watumu 15 meit ke kampus")
+elif transportasi == "jalan kaki":
+    print("waktumu lebih lama 30 menit")
+else:
+    ("makin lama waktumu ke kampus")
+
+#gagal total kak waktunya kurang 

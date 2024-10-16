37ca2d7682f3e436e500dd304464a299a8fe3694
Muhammad Yusri <135690839+muhammadyusri121@users.noreply.github.com>
2024-10-16 23:02:13 +0700

    Delete UTS/UTS_240441100155_SuciRamadhaniArrasyid.py

--- UTS/UTS_240441100155_SuciRamadhaniArrasyid.py
+++
@@ -1,8 +0,0 @@
-makan = input("apakah kamu sudah makan? ")
-if makan:
-    print("iya")
-else :
-    print("tidak")
-mandi= input("apakah kamu sudah mandi?")
-transportasi= input("transportasi apa yang akan di gunakan?")
-    

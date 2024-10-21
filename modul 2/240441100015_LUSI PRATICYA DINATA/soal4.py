6a36c5ca6b4136bda4a8c817b21f884445187f6f
Muhammad Yusri <135690839+muhammadyusri121@users.noreply.github.com>
2024-10-21 19:51:31 +0700

    Delete modul 2 directory

--- modul 2/240441100015_LUSI PRATICYA DINATA/soal4.py
+++
@@ -1,8 +0,0 @@
-# Input dari pengguna
-tahun = int(input("Masukkan tahun: "))
-
-# Menentukan apakah tahun kabisat atau tidak
-if (tahun % 4 == 0 and tahun % 100 != 0) or (tahun % 400 == 0):
-    print(f"{tahun} adalah tahun kabisat.")
-else:
-    print(f"{tahun} bukan tahun kabisat.")
--- modul 2/modul2_240441100095_liatulmukarromah/tugas4.py
+++
@@ -1,9 +0,0 @@
-tahun = int(input("masukkan tahun: "))
-
- #seleksi tahun untuk menetukan tahun kabisat
-if (tahun % 4 == 0 and tahun % 100 != 0) or (tahun % 400 == 0):
-    print(f"{tahun} merupakan tahun kabisat")
-else:
-    print(f"{tahun} bukan tahun kabisat")
-
- 

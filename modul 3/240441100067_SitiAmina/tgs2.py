9ca17a3babd4c1b45328813510cae8bfdd1259c6
Muhammad Yusri <135690839+muhammadyusri121@users.noreply.github.com>
2024-10-21 19:51:41 +0700

    Delete modul 3 directory

--- modul 3/240441100067_SitiAmina/tgs2.py
+++
@@ -1,9 +0,0 @@
-
-angka = int(input("Masukkan angka : "))
-string = str(angka)
-terbalik = ""
-
-for i in range(len(string)-1, -1, -1):
-    terbalik += string[i]
-terbalik = int(terbalik)
-print("setelah dibalik :",terbalik)
--- modul 3/240441100139_AdyttaPutraTarigan_Modul3_AsprakkakSarah/soal2.py
+++
@@ -1,7 +0,0 @@
-angka = input("Masukkan angka bulat: ")
-hasil = ""
-
-for digit in angka:
-    hasil = digit + hasil
-
-print("Urutan angka setelah dibalik: ", hasil)
